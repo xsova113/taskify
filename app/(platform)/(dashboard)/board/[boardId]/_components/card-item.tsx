3c920260656291cf505316001f6264fcbcd0151c
George <xsova113@gmail.com>
2023-11-20 21:41:35 -0800

    udpate card and list reorder functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
 
 interface CardItemProps {
@@ -9,11 +10,18 @@ interface CardItemProps {
 
 export const CardItem = ({ data, index }: CardItemProps) => {
   return (
-    <div
-      role="button"
-      className="truncate rounded-md border-2 border-transparent bg-white px-3 py-2 text-sm shadow-sm hover:border-black"
-    >
-      {data.title}
-    </div>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="truncate rounded-md border-2 border-transparent bg-white px-3 py-2 text-sm shadow-sm hover:border-black"
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   );
 };
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -6,6 +6,7 @@ import { ElementRef, useRef, useState } from "react";
 import { CardForm } from "./card-form";
 import { cn } from "@/lib/utils";
 import { CardItem } from "./card-item";
+import { Draggable, Droppable } from "@hello-pangea/dnd";
 
 interface ListItemProps {
   index: number;
@@ -28,27 +29,45 @@ export const ListItem = ({ data, index }: ListItemProps) => {
   };
 
   return (
-    <li className="h-full w-[272px] shrink-0 select-none">
-      <div className="w-full rounded-md bg-[#f1f2f4] pb-2 shadow-md">
-        <ListHeader onAddCard={enableEditing} data={data} />
-        <ol
-          className={cn(
-            "mx-1 flex flex-col gap-y-2 px-1 py-0.5",
-            data.cards.length > 0 ? "mt-2" : "mt-0",
-          )}
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <li
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          className="h-full w-[272px] shrink-0 select-none"
         >
-          {data.cards.map((card, index) => (
-            <CardItem index={index} key={card.id} data={card} />
-          ))}
-        </ol>
-        <CardForm
-          listId={data.id}
-          ref={textAreaRef}
-          isEditing={isEditing}
-          enableEditing={enableEditing}
-          disableEditing={disableEditing}
-        />
-      </div>
-    </li>
+          <div
+            {...provided.dragHandleProps}
+            className="w-full rounded-md bg-[#f1f2f4] pb-2 shadow-md"
+          >
+            <ListHeader onAddCard={enableEditing} data={data} />
+            <Droppable droppableId={data.id} type="card">
+              {(provided) => (
+                <ol
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className={cn(
+                    "mx-1 flex flex-col gap-y-2 px-1 py-0.5",
+                    data.cards.length > 0 ? "mt-2" : "mt-0",
+                  )}
+                >
+                  {data.cards.map((card, index) => (
+                    <CardItem index={index} key={card.id} data={card} />
+                  ))}
+                  {provided.placeholder}
+                </ol>
+              )}
+            </Droppable>
+            <CardForm
+              listId={data.id}
+              ref={textAreaRef}
+              isEditing={isEditing}
+              enableEditing={enableEditing}
+              disableEditing={disableEditing}
+            />
+          </div>
+        </li>
+      )}
+    </Draggable>
   );
 };
